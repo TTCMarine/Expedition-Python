a9da4a97500b8703cfd83cecae33b7f4d3aa4405
Tom Cheney <tom@tomcheney.com>
2025-04-17 12:48:21 +0100

    use the autogen docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,3 @@
-import sys
-import os
-import logging
-logging.basicConfig(level=logging.DEBUG)
-sys.path.insert(0, os.path.abspath('../'))
-
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
